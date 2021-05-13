bf8e3cd5ae2d2b8a0de59f54a5fdfb407cd04c67
lijiamao1996 <1065081614@qq.com>
2021-05-13 16:32:26 +0800

    项目初始结构创建

---
+++ gulimall-ware/src/test/java/com/arthur/gulimall/ware/GulimallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthur.gulimall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
