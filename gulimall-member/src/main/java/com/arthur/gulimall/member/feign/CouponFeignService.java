16df932372f2a02b96491e9e03f8675d66f18a3d
lijiamao1996 <1065081614@qq.com>
2021-05-14 23:06:03 +0800

    20215.14

---
+++ gulimall-member/src/main/java/com/arthur/gulimall/member/feign/CouponFeignService.java
@@ -0,0 +1,11 @@
+package com.arthur.gulimall.member.feign;
+
+import com.arthur.common.utils.R;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@FeignClient("gulimall-coupon")
+public interface CouponFeignService {
+    @RequestMapping("/coupon/coupon/member/list")
+    public R membercoupons();
+}
--- gulimall-product/src/test/java/com/arthur/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/arthur/gulimall/product/GulimallProductApplicationTests.java
@@ -1,13 +1,32 @@
 package com.arthur.gulimall.product;
 
+import com.arthur.gulimall.product.entity.BrandEntity;
+import com.arthur.gulimall.product.service.BrandService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class GulimallProductApplicationTests {
 
+    @Autowired
+    BrandService brandService;
+
     @Test
     void contextLoads() {
+//        BrandEntity brandEntity = new BrandEntity();
+//        brandEntity.setBrandId(6L);
+//        brandEntity.setName("华为");
+//        brandEntity.setName("阿里巴巴");
+//        brandService.save(brandEntity);
+//        System.out.println("ok");
+        List<BrandEntity> list = brandService.list(new QueryWrapper<BrandEntity>().eq("name", "华为"));
+        list.forEach((item)->{
+            System.out.println(item);
+        });
     }
 
 }
